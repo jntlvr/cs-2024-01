eeb77a2ed560bd7b836c8412be4f9afd36c87261
jntlvr <131724696+jntlvr@users.noreply.github.com>
2024-04-21 23:00:36 -0300

    Construção de classes

---
+++ endereco/src/main/java/com/github/jntlvr/Logradouro.java
@@ -0,0 +1,6 @@
+package com.github.jntlvr;
+
+public class Logradouro {
+    private String nome;
+    private TipoLogradouro tipoLogradouro;
+}
--- endereco2/src/main/java/com/github/jntlvr/Logradouro.java
+++
@@ -1,4 +0,0 @@
-package com.github.jntlvr;
-
-public class Logradouro {
-}
